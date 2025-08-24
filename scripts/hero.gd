9c4a234a1a65eb852b589022cab549ce8ae450ff
Tanner <devtanc@gmail.com>
2025-08-23 21:58:02 -0600

    move and attack animations

--- scripts/hero.gd
+++ scripts/hero.gd
@@ -1,13 +1,32 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 100
+var blocking_animation: bool = false
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-	if input_direction.x != 0:
-		$Character.flip_h = input_direction.x < 0
+	if Input.is_action_just_pressed("attack"):
+		blocking_animation = true
+		$Character.play("attack")
+		pass
+	else:
+		var input_direction = Input.get_vector("left", "right", "up", "down")
+		velocity = input_direction * speed
+		
+		if input_direction.x != 0:
+			$Character.flip_h = input_direction.x < 0
+
+		if velocity.x != 0 || velocity.y != 0:
+			$Character.play("walk")
+		else:
+			$Character.play("default")
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	if blocking_animation:
+		pass
+	else:
+		get_input()
+		move_and_slide()
+
+
+func _on_character_animation_finished() -> void:
+	blocking_animation = false
