afce540c8201d22fb68d074203d8e17ed781b1c4
Tanner <devtanc@gmail.com>
2025-08-23 21:35:04 -0600

    change direction when moving

--- scripts/hero.gd
+++ scripts/hero.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	if input_direction.x != 0:
+		$Character.flip_h = input_direction.x < 0
 
 func _physics_process(delta):
 	get_input()
